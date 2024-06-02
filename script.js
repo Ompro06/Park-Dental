0388e45f30ab3f7c593d1c66fe7218cf36e76206
Ompro06 <148696992+Ompro06@users.noreply.github.com>
2024-06-02 21:23:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loader = document.querySelector('.loader');
+
+    
+    setTimeout(() => {
+        loader.style.display = 'none';
+    }, 5000);
+});
